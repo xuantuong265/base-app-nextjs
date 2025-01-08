ffa7ab6d80b8a46355026b280c75d192bbd01350
tuongbx <tuong.bx@metatechnologylab.io>
2025-01-08 17:04:11 +0700

    add some config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
